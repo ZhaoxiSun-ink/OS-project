d48078564917c723dc63fec4b0d38c1c3b7c4132
sunzhaoxi99 <sunzhaoxi99@outlook.com>
2020-07-04 13:01:40 -0400

    input argument

--- p1.py
+++ p1.py
@@ -1,1 +1,17 @@
 #Summer 2020 CSCI 4210 Operating Systems project 1
+import sys #get argument
+
+#main part
+if __name__ == '__main__':
+	if len(sys.argv) != 8:
+		print("ERROR: Invalid argument.")
+		sys.exit(2)
+
+	number_process = int(sys.argv[1])
+	seed = float(sys.argv[2])
+	parameter = float(sys.argv[3])
+	upper_bound = float(sys.argv[4])
+	tcs = float(sys.argv[5])
+	alpha = float(sys.argv[6])
+	tslice = float(sys.argv[7])
+	rradd = float(sys.argv[8])
